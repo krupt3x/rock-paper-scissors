349bdeb1d60c0f4ce3f1d29ba851ac903716ace1
krupt3x <161846850+krupt3x@users.noreply.github.com>
2024-05-04 11:29:49 +0530

    implement the logic to get the user's (humans) choice

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// randomly generates rock, paper, scissors for the computer. 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     let randIndex = Math.floor(Math.random() * choices.length);
     let compChoice = choices[randIndex];
-    return compChoice;
+    return compChoice.toLowerCase();
+}
+
+// takes the human player's choice and returns it back in lower case.
+function getHumanChoice() {
+    const choice = prompt("enter your choice: rock, paper, scissors");
+    return choice.toLowerCase();
 }
 
 console.log(getComputerChoice());
-
+console.log(getHumanChoice())
 module.exports = getComputerChoice
